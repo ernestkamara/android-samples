89dd69c6d2676a7249eb7dc8e7c2e98678789f77
Ernest Saidu Kamara <ernest@kamara.io>
2020-01-01 16:26:18 +0100

    Upgraded gradle and added navigation graph
    Upgraded gradle to 6.1-milestone-2 and added navigation graph along with dependencies refactoring.

---
+++ app/src/main/java/io/kamara/android/samples/fragments/MainFragment.kt
@@ -0,0 +1,18 @@
+package io.kamara.android.samples.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.kamara.android.samples.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
